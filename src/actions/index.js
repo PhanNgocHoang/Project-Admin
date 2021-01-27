6ad1925f49db36c6b02ac98f802151d863c29c0c
Hoang Phan Ngoc <hoang.phan.intern@ntq-solution.com.vn>
2021-01-27 16:17:07 +0700

    update author

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,4 @@
 import * as types from './../constants/actionTypes'
-export const listAllTypeBook = () => {
-    return {
-        type: types.LIST_ALL_TYPE_BOOK
-    }
-}
 export const getDataLogin = () => {
     return {
         type: types.DATA_LOGIN
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,5 @@
 import { combineReducers } from 'redux'
-import typesbook from './typesbook.reducers'
 import login from './login.reducer'
 export const myReducers = combineReducers({
-    typesbook,
     login
 })
